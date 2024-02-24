a9a9a3d8921c8cd4efcc2d368680d79ee725f2e7
Brandon Braner <brandon.braner@gmail.com>
2024-02-24 17:34:06 -0600

    removed the code copilot put in

--- problems/two_crystal_balls/crystal_balls.py
+++ problems/two_crystal_balls/crystal_balls.py
@@ -2,10 +2,6 @@ import math
 from typing import List
 
 
-# def crystal_balls(floors: List[int]) -> int:
-#     return math.ceil((math.sqrt(1 + 8 * floors) - 1) / 2)
-
-
 def crystal_balls(floors: List[bool]) -> int:
     num_of_floors = len(floors) - 1
     jump_distance = math.floor(math.sqrt(len(floors)))
@@ -20,10 +16,8 @@ def crystal_balls(floors: List[bool]) -> int:
     # go back to the previous point we knew the ball didn't break and drop it till we find where it breaks
     # since the endpoint is exclusive we need to add one to make sure we check the floor it broke on just incase
     # that is the first floor it breaks on
-    for i in range(breaking_point - jump_distance, breaking_point+1):
+    for i in range(breaking_point - jump_distance, breaking_point + 1):
         if floors[i]:
             return i
 
     return -1
-
-
