34e67bec8756ad2f919578a377e21b03beaca6dc
Brandon Braner <brandon.braner@gmail.com>
2024-02-24 18:26:09 -0600

    bubble sort in desc

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -4,10 +4,16 @@ from typing import List
 def bubble_sort(arr: List[int]) -> List[int]: from typing import List
 
 
-def bubble_sort(arr: List[int]) -> List[int]:
+def bubble_sort(arr: List[int], direction='asc') -> List[int]:
     n = len(arr)
-    for i in range(n):
-        for j in range(n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    if direction == 'asc':
+        for i in range(n):
+            for j in range(n - i - 1):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    else:
+        for i in range(n):
+            for j in range(n - i - 1):
+                if arr[j] < arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
--- bubble_sort/test_buuble_sort.py
+++ bubble_sort/test_buuble_sort.py
@@ -3,7 +3,12 @@ from bubble_sort.bubble_sort import bubble_sort
 
 class TestBubbleSort:
 
-    def test_list_with_values(self):
+    def test_list_with_values_asc(self):
         assert bubble_sort([3, 2, 1]) == [1, 2, 3]
 
         assert bubble_sort([3, 2, 1, 7, 10, 100, 1007]) == [1, 2, 3, 7, 10, 100, 1007]
+
+    def test_list_with_values_desc(self):
+        assert bubble_sort([1, 7, 3, 2, 1], direction='desc') == [7, 3, 2, 1, 1]
+
+        assert bubble_sort([3, 2, 1, 7, 10, 100, 1007], direction='desc') == [1007, 100, 10, 7, 3, 2, 1]
